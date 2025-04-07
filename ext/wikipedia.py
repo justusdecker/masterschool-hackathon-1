a5c3aec9830fa79b9611fbc6742b14f77d9516bd
justusdecker <justus.d2025@gmail.com>
2025-04-07 11:05:49 +0200

    0.3

--- ext/wikipedia.py
+++ ext/wikipedia.py
@@ -1,21 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
-def get_wiki(search):
-    print("result:\n")
-    req = requests.get(f"https://de.wikipedia.org/wiki/{search}")
-    BeautifulSoup().find()
-    soup = BeautifulSoup(req.content, "html.parser")
-    
-    found = soup.find_all("div", class_="mw-content-ltr")
-    found = soup.find_all("a")
-    links = {}
-    for i in found:
-        link,text = i.get("href"),i.get("contents")
-        if not link: continue
-        
-        if not i.attrs['href']: continue
-        links[str(i.attrs['href'])] = i.text
-    print(links)
-    
 
-get_wiki("Minecraft")
+"""
+WFAPI - Wikipedia fetching API
+(c) 2025 - Justus Decker
+"""
+
+def get_soup(link:str) -> BeautifulSoup:
+    """
+    Get the Website content with HTML Tags
+    """
+    return BeautifulSoup(
+        requests.get(f"{link}").content, 
+        "html.parser"
+        )
+def get_wiki_text(link: str) -> str:
+    soup = get_soup(link)
+    return soup.text    #Remove HTML text. Returns only text
+
+def get_wiki_links(link: str) -> list[str,str]:
+    soup = get_soup(link)
+    found = soup.find_all("a")
+    links = []
+    for i in found:
+        link = i.get("href")
+        
+        if not link: continue
+        if not i.attrs['href']: continue
+        
+        links.append((str(i.attrs['href']),i.text)) #HREF Link & the text in the current element
+        
+    return links
+
+print(get_wiki_text("https://de.wikipedia.org/wiki/Minecraft"))
--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 """http://de.wikipedia.org/wiki/Minecraft"""
-
+import pygame as pg
+class App:
+    WIDTH = 1280
+    HEIGHT = 720
+    WINDOW = pg.display.set_mode((WIDTH,HEIGHT))
+    is_running = True
+    def __init__(self):
+        pass
+    def run(self):
+        while self.is_running:
+            self.WINDOW.fill(pg.Color("#e85f58"),(0,0,self.WIDTH // 2,self.HEIGHT))
+            self.WINDOW.fill(pg.Color("#58c8e8"),(self.WIDTH // 2,0,self.WIDTH,self.HEIGHT))
+            
+            pg.display.update()
+            self.check_events()
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+if __name__ == "__main__":
+    APP = App()
+    APP.run()
